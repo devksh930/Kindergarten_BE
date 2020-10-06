3414805ed11baa52e2c1e217665c28ef33cde657
devksh930 <ksh930@icloud.com>
2020-10-07 04:54:17 +0900

    REFACTOR: package 정리 및 인증,권한 에러 기능 리팩토링, ExceptionContorller 생성 #10

--- src/main/java/com/kindergarten/api/security/salt/SaltRepository.java
+++ src/main/java/com/kindergarten/api/security/salt/SaltRepository.java
@@ -1,6 +1,4 @@
-package com.kindergarten.api.security.repository;
-
-import com.kindergarten.api.security.entitiy.Salt;
+package com.kindergarten.api.security.salt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
