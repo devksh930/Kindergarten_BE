b9d23398f620663fcb0e6bfed72d6df43a180dfe
devksh930 <ksh930@icloud.com>
2020-10-13 01:58:58 +0900

    MODIFY : 회원가입 valdaiton 어노테이션 간략화 #2

--- src/main/java/com/kindergarten/api/model/dto/UserDTO.java
+++ src/main/java/com/kindergarten/api/model/dto/UserDTO.java
@@ -10,22 +10,22 @@ import java.util.List;
 public class UserDTO {
     @Data
     public static class Create {
-        @NotBlank(message = "비어 있어서는 안됩니다.")
         @Min(value = 6, message = "6자 이상 입력해주세요")
         @Max(value = 20, message = "20자 이상 입력할수 없습니다")
         private String userid;
-        @NotBlank(message = "비어 있어서는 안됩니다.")
-        @Min(value = 8, message = "8자 이상 입력해주세요")
-        @Max(value = 20, message = "20자 이상 입력할수 없습니다")
+
         @Pattern(regexp = "[a-zA-Z1-9]{8,20}", message = "비밀번호는 영어와 숫자로 포함해서 8~20자리 이내로 입력해주세요.")
         private String password;
-        @NotBlank(message = "비어 있어서는 안됩니다.")
+
         @Min(value = 2, message = "2자 이상 입력해주세요")
         private String name;
+
         @Pattern(regexp = "\\d{10,11}", message = "숫자만 입력가능합니다.")
         private String phone;
+
         @Email(message = "올바른 이메일을 입력해주세요 aaa@email.com")
         private String email;
+
         private String role;
         private String kindergarten_id;
         private List<ADD_Student> student = new ArrayList<>();
