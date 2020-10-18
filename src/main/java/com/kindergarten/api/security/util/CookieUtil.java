f0d43188a57f44abf182c73ceb84b5bec6853edd
devksh930 <ksh930@icloud.com>
2020-10-19 00:49:13 +0900

    FIX : 개발시 httpOnly false 설정

--- src/main/java/com/kindergarten/api/security/util/CookieUtil.java
+++ src/main/java/com/kindergarten/api/security/util/CookieUtil.java
@@ -11,7 +11,8 @@ public class CookieUtil {
 
     public Cookie createCookie(String cookieName, String value) {
         Cookie token = new Cookie(cookieName, value);
-        token.setHttpOnly(true);
+        token.setHttpOnly(false);
+//        token.setHttpOnly(true);
         token.setMaxAge((int) JwtUtil.TOKEN_VALIDATION_SECOND);
         token.setPath("/");
         return token;
